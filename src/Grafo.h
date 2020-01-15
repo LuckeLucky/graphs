8123401f84cb05380c74a4832e4bc3918122234a
LuckeLucky <43279191+LuckeLucky@users.noreply.github.com>
2020-01-15 00:02:53 +0000

    Rename Grafo.h to src/Grafo.h

--- src/Grafo.h
+++ src/Grafo.h
@@ -23,41 +23,41 @@ public:
 	~Grafo();
 	//Carregar os dados de ficheiros (do grafo e de pessoas);
 	bool Load(const string &fich_grafo, const string &fich_pessoas);
-	//Contar o n˙mero de nÛs/fronteiras do grafo;
+	//Contar o n√∫mero de n√≥s/fronteiras do grafo;
 	int ContarNos();
-	//Contar o n˙mero de arestas/arcos do grafo;
+	//Contar o n√∫mero de arestas/arcos do grafo;
 	int ContarArcos();
-	//Determinar toda a memÛria ocupada;
+	//Determinar toda a mem√≥ria ocupada;
 	int Memoria();
-	//Determinar qual o nÛ/fronteira que tem mais arcos/aresta, se existirem v·rios devolve uma lista deles;
+	//Determinar qual o n√≥/fronteira que tem mais arcos/aresta, se existirem v√°rios devolve uma lista deles;
 	list<int> *NoMaisArcos();
-	//Verificar se um nÛ È adjacente de outro nÛ do Grafo;
+	//Verificar se um n√≥ √© adjacente de outro n√≥ do Grafo;
 	bool Adjacencia(int v1, int v2);
-	//Determinar um caminho(n„o interessa se È o mais r·pido!, È qualquer um!) de um nÛ/fronteira para outro.... //devolvendo o custo total;
+	//Determinar um caminho(n√£o interessa se √© o mais r√°pido!, √© qualquer um!) de um n√≥/fronteira para outro.... //devolvendo o custo total;
 	list<int> *Caminho(int v1, int v2, double &custo_total);
-	//Determinar quais os vÈrtices que est„o isolados (Um vÈrtice È isolado se n„o existe nenhum caminho atÈ ele!);
+	//Determinar quais os v√©rtices que est√£o isolados (Um v√©rtice √© isolado se n√£o existe nenhum caminho at√© ele!);
 	list<int> *VerticesIsolados();
-	//Verificar se um dado vÈrtice existe
+	//Verificar se um dado v√©rtice existe
 	bool Search(int v);
-	//Remover um dado vÈrtice, tambÈm ser· necess·rio remover todas as arestas;
+	//Remover um dado v√©rtice, tamb√©m ser√° necess√°rio remover todas as arestas;
 	bool RemoverVertice(int v);
-	//Remover a aresta que liga 2 vÈrtices;
+	//Remover a aresta que liga 2 v√©rtices;
 	bool RemoverAresta(int v1, int v2);
-	//Gravar para ficheiro em formato XML todas as informaÁıes do Grafo;
+	//Gravar para ficheiro em formato XML todas as informa√ß√µes do Grafo;
 	void EscreverXML(const string &s);
-	//Ler de um ficheiro em formato XML todo o Grafo (antes de ler deve ser apagado tudo o que estiver no Grafo); Se leu corretamente devolve //true, sen„o devolve false;
+	//Ler de um ficheiro em formato XML todo o Grafo (antes de ler deve ser apagado tudo o que estiver no Grafo); Se leu corretamente devolve //true, sen√£o devolve false;
 	bool LerXML(const string &s);
-	//Devolver uma lista de todos os vÈrtices/fronteiras de um dado tipo;
+	//Devolver uma lista de todos os v√©rtices/fronteiras de um dado tipo;
 	list<int> *DevolveVerticesTipo(const string &tipo);
-	//Determinar o caminho mÌnimo entre 2 nÛs, devolvendo o custo total;
+	//Determinar o caminho m√≠nimo entre 2 n√≥s, devolvendo o custo total;
 	list<int> *CaminhoMinimo(int v1, int v2, double &custo_total);
-	//Determinar o caminho m·ximo entre 2 nÛs (passando somente uma vez em cada vÈrtice), devolvendo o custo total;
+	//Determinar o caminho m√°ximo entre 2 n√≥s (passando somente uma vez em cada v√©rtice), devolvendo o custo total;
 	list<int> *CaminhoMaximo(int v1, int v2, double &custo_total);
-	//Ser· possÌvel ir de uma fronteira(v1) a outra(v2), passando somente por fronteiras de um dado tipo?
+	//Ser√° poss√≠vel ir de uma fronteira(v1) a outra(v2), passando somente por fronteiras de um dado tipo?
 	bool PossivelCaminho(int v1, int v2, int TipoFronteira);
-	//Assumindo que todas as pessoas (do ficheiro) andaram em viagem, qual foi a fronteira com menos tr·fego/visitas? (No caso de serem v·rias,// deve devolver uma lista com as fronteiras);
+	//Assumindo que todas as pessoas (do ficheiro) andaram em viagem, qual foi a fronteira com menos tr√°fego/visitas? (No caso de serem v√°rias,// deve devolver uma lista com as fronteiras);
 	void FronteirasMenosVisitadas(list<int> &Lv);
-	//Dada uma fronteira (v1), determinar todas as fronteiras onde n„o È possÌvel chegar apartir de v1. Deve devolver uma lista dessas fronteiras!;
+	//Dada uma fronteira (v1), determinar todas as fronteiras onde n√£o √© poss√≠vel chegar apartir de v1. Deve devolver uma lista dessas fronteiras!;
 	void SitiosInacessiveisAPartirDe(int V1, list<int> &Lv);
 private:
 	//Insere no map_caminhos a aresta lida do ficheiro
